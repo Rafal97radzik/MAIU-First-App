42a12c4e7f279403955c65f178aed7a9ec663e2f
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2022-06-13 22:31:26 +0200

    Dodanie strony surprise

--- Katsumi/Pages/SurprisePage.xaml.cs
+++ Katsumi/Pages/SurprisePage.xaml.cs
@@ -6,4 +6,16 @@ public partial class SurprisePage : ContentPage
 	{
 		InitializeComponent();
     }
+
+    //Nie dzia³a
+    private void ButtonGoBackClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("..");
+    }
+
+    //Nie dzia³a
+    private void ButtonGoBackV2Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.Navigation.PopAsync();
+    }
 }
